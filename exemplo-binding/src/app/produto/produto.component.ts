6b3c3c971cdd484191caa3dfece06573c6e4832e
aledaveiga <alesandrodaveiga@gmail.com>
2020-03-07 13:27:10 -0300

    Utilizando recursos de event binding e two-way-databinding.

--- exemplo-binding/src/app/produto/produto.component.ts
+++ exemplo-binding/src/app/produto/produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-produto',
@@ -13,9 +14,26 @@ export class ProdutoComponent implements OnInit {
   url: string = "http://apexensino.com.br";
   imagem: string = "https://picsum.photos/150/70";
 
+  campo: string = "Apex";
+  gostandoDeAngular: boolean = true;
+  cor: string = "vermelho";
+  numero: number = 3;
+
+  numeros: number[] = [1,2,3,4,5,6,7,8,9];
+  cidades: string[] = ["Florianópolis", "São José", "Palhoça", "Biguaçu", "Rio Vermelho"];
+  fonte: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  alterarNumero(event): void {
+    this.numero = event.target.value;
+  }
+
+  avisar(): void {
+    alert("Seja bem vindo! " + this.campo)
   }
 
 }
